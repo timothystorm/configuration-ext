db6479a7b624330791edb119b5f51511f8b3c168
timothystorm <timothystorm@gmail.com>
2016-02-12 21:29:26 -0700

    Externalize schema
    Moved the schema definition out of the RuntimeConfigurationHandler class
    into META-INF so upgraded schemas can be configured without modifying
    code.

--- src/main/java/commons/configuration/ext/RuntimeConfiguration.java
+++ src/main/java/commons/configuration/ext/RuntimeConfiguration.java
@@ -1,8 +1,6 @@
 package commons.configuration.ext;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
 import java.net.URL;
